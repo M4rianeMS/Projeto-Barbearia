932846ccf17afc8022abc10fe039b1977b5eec64
M4rianeMS <mmariane691@gmail.com>
2026-01-18 14:52:30 -0300

    Arquitetura principal

---
+++ src/main/java/com/example/projetobarbearia/ProjetobarbeariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetobarbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetobarbeariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetobarbeariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetobarbearia/ProjetobarbeariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetobarbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetobarbeariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
